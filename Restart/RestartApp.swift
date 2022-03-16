ed8663268ce4049a0dbdb7e395b09df8f1ad7553
Md. Shah Faishal <abadlas1987@gmail.com>
2022-03-16 16:21:21 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Scrambleapps on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
